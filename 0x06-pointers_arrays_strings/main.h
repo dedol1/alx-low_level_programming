e573e18d8d6daf8f9f6faf41def2bc33fcc0de86
dedol1 <afoteylaryeasamuel@gmail.com>
2022-06-16 04:28:52 -0700

    these files are important filesfor the assignment

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void positive_or_negative(int n);
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
+int _putchar(char c);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+
+#endif /* MAIN_H */
